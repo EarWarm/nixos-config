8b10423429398c48f302094442823847cfe395fc
EarWarm <EarWarm@bk.ru>
2025-07-17 15:17:37 +0500

    unstable-0.0.1

--- configuration.nix
+++ configuration.nix
@@ -7,9 +7,12 @@
         # Import components
         ./components/datetime.nix
         ./components/fonts.nix
+        ./components/rofi.nix
         ./components/desktop.nix
         ./components/waybar.nix
         # Import programms
+        ./programms/git.nix
+        ./programms/firefox.nix
     ];
 
 }
