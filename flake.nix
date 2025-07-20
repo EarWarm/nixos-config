8adc5dfb4cb8f2fde7e32bcb400c8fbfcaecc95e
EarWarm <EarWarm@bk.ru>
2025-07-21 00:16:41 +0500

    change hostname

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     let
       system = "x86_64-linux";
     in {
-      nixosConfigurations.my-host = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
 
         modules = [
